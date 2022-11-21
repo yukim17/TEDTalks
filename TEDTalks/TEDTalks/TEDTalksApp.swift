481b238ba563338671081c340a20c9321c8170f0
egrishina <egrishina22@fed.idserve.net>
2022-11-21 11:56:21 +0100

    Project setup

---
+++ TEDTalks/TEDTalks/TEDTalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEDTalksApp.swift
+//  TEDTalks
+//
+//  Created by Ekaterina Grishina on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TEDTalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
